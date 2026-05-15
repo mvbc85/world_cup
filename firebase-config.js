8ca75a7f131c28eb24ff0efd79cf1a0741864768
mvbc85 <mvbc85@gmail.com>
2026-05-15 18:25:51 +0800

    Deploy GitHub Pages

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBIPFbaTsSjbxUFnbjCqok7KB4Nt_Vl6-s",
+  authDomain: "world-cup-sweepstake-10903.firebaseapp.com",
+  projectId: "world-cup-sweepstake-10903",
+  storageBucket: "world-cup-sweepstake-10903.firebasestorage.app",
+  messagingSenderId: "271751004127",
+  appId: "1:271751004127:web:27e1e857d3fc5b83bd8e6a"
+};
+
+window.APP_ENTRY_PASSKEY = "DONUTS";
